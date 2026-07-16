c50801543469d9db192bd19907eb8a391081bfd3
Dharsan k <thehonoredone672@gmail.com>
2026-07-16 22:38:23 +0530

    Fix Button component path casing for Linux

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,5 +1,5 @@
 import Input from "../../components/common/Input/Input";
-import Button from "../../components/common/Button/Button";
+import Button from "../../components/common/button/Button";
 
 export default function Register() {
   return <h2>Register Page</h2>;
